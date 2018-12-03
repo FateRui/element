43352af9073fd55cdd32e8a0ae0f4eef69cb2e57
ZPJ <zhou1050253145@163.com>
2018-12-04 02:22:10 +0800

    封装了linetitle组件,star组件,商家介绍组件

---
+++ src/components/lineTitle/lineTitle.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="line-title">
+      <div class="line" ref="left"></div>
+      <div class="title" v-text="text"></div>
+      <div class="line" ref="right"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String
+    },
+    space: {
+      type: String,
+      default: '0'
+    },
+    color: {
+      type: String,
+      default: 'rgb(0,0,0)'
+    }
+  },
+  mounted () {
+    this.$refs.left.style.marginRight = this.space
+    this.$refs.right.style.marginLeft = this.space
+    this.$refs.left.style.backgroundColor = this.color
+    this.$refs.right.style.backgroundColor = this.color
+  }
+}
+</script>
+
+<style scoped lang="less">
+  .line-title{
+    display: flex;
+    line-height: 14px;
+    height: 14px;
+    .title{
+      font-size: 14px;
+      line-height: 14px;
+      color: rgba(255,255,255);
+      font-weight: 700;
+    }
+    .line{
+      flex: 1;
+      margin-top: 7px;
+      height: 1px;
+      transform: scaleY(0.33);
+    }
+  }
+  .border-bottom-1px(@color){
+    position: relative;
+    &:after{
+      content: '';
+      display: block;
+      width: 100%;
+      position: absolute;
+      height: 1px;
+      left: 0;
+      bottom: 0;
+      background-color:@color;
+      transform: scaleY(0.33);
+    }
+  }
+</style>
--- src/components/sellerProduce/sellerProduce.vue
+++ src/components/sellerProduce/sellerProduce.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="sellerProduce" v-if="show">
-    <div class="product-warpper">
-      <div class="content">
-        <h1 class="name" v-text="seller.name"></h1>
-        <div class="star-warpper"><star size=48 :score='seller.score'></star></div>
-      </div>
+  <transition name="fade">
+    <div class="sellerProduce" v-if="show">
+        <div class="product-warpper">
+          <div class="content">
+            <h1 class="name" v-text="seller.name"></h1>
+            <div class="star-warpper"><star size=48 :score='seller.score'></star></div>
+            <div class="title-warpper"><line-title text="优惠信息" space="12px" color="rgba(255,255,255,.2)"></line-title></div>
+            <div class="bulletin-list">
+              <ul>
+                <li v-for="(item,index) in seller.supports" :key="index" class="bulletin-item">
+                  <bullentin-icon :type="item.type" size="16"></bullentin-icon><span class="text" v-text="item.description"></span>
+                </li>
+              </ul>
+            </div>
+            <div class="title-warpper"><line-title text="商家公告" space="12px" color="rgba(255,255,255,.2)"></line-title></div>
+            <div class="text" v-text="seller.bulletin"></div>
+          </div>
+        </div>
+        <div class="icon-close close" @click="toggleMe"></div>
     </div>
-    <div class="icon-close close" @click="toggleMe"></div>
-  </div>
+  </transition>
 </template>
 
 <script>
 import star from '../../components/star/star'
+import lineTitle from '../../components/lineTitle/lineTitle'
+import bullentinIcon from '../../components/bullentinIcon/bullentinIcon'
 export default {
   data () {
     return {
@@ -28,7 +42,7 @@ export default {
       type: Object
     }
   },
-  components: {star}
+  components: {star, lineTitle, bullentinIcon}
 }
 </script>
 
@@ -36,18 +50,26 @@ export default {
   @import "../../common/less/iconfont";
   .sellerProduce{
     position: fixed;
+    overflow: auto;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
     background:rgba(7,17,27,.8);
+    backdrop-filter: blur(10px);
+    &.fade-enter,&fade-leave-to{
+      opacity: 0;
+    }
+    &.fade-enter-active,&fade-leave-active{
+      transition: all .5s;
+    }
     .product-warpper{
       margin: 0 36px;
       min-height: 100%;
       .content{
         padding-bottom: 64px;
         .name{
-          margin-top: 64px;
+          padding-top: 64px;
           text-align: center;
           font-size: 16px;
           line-height: 16px;
@@ -58,6 +80,37 @@ export default {
           text-align: center;
           margin-top: 16px;
         }
+        .title-warpper{
+          margin-top: 28px;
+        }
+        .bulletin-list{
+          margin-top: 24px;
+          padding: 0 12px;
+          .bulletin-item{
+            font-size: 12px;
+            line-height: 12px;
+            font-weight: 200;
+            color: rgb(255,255,255);
+            margin-bottom: 12px;
+            &:last-child{
+              margin-bottom: 0;
+            }
+            span{
+              vertical-align: middle;
+              &:last-child{
+                margin-left: 6px;
+              }
+            }
+          }
+        }
+        .text{
+          margin-top: 24px;
+          padding: 0 12px;
+          font-size: 12px;
+          color: rgb(255,255,255);
+          font-weight: 200;
+          line-height: 24px;
+        }
       }
     }
     .close{
@@ -66,7 +119,7 @@ export default {
       color: rgba(255,255,255,.5);
       text-align: center;
       position: relative;
-      top: -64px;
+      top: -32px;
       left: 0;
     }
   }
